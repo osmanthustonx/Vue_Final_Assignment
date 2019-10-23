4aba6e848d7cf8a6807b8ff4d47b88c0d98eb59c
BrianKuei <vx3012564897@gmail.com>
2019-10-23 10:51:24 +0800

    CoutomerOrder 修改表單驗證版型

--- src/components/pages/CustomerOrders.vue
+++ src/components/pages/CustomerOrders.vue
@@ -138,16 +138,16 @@
             <th>單價</th>
           </thead>
           <tbody>
-            <tr v-for="item in order.products" :key="item.id">
+            <!-- <tr v-for="item in order.products" :key="item.id">
               <td class="align-middle">{{ item.product.title }}</td>
               <td class="align-middle">{{ item.qty }}/{{ item.product.unit }}</td>
               <td class="align-middle text-right">{{ item.final_total }}</td>
-            </tr>
+            </tr> -->
           </tbody>
           <tfoot>
             <tr>
               <td colspan="2" class="text-right">總計</td>
-              <td class="text-right">{{ order.total }}</td>
+              <!-- <td class="text-right">{{ order.total }}</td> -->
             </tr>
           </tfoot>
         </table>
@@ -156,32 +156,32 @@
           <tbody>
             <tr>
               <th width="100">Email</th>
-              <td>{{ order.user.email }}</td>
+              <!-- <td>{{ order.user.email }}</td> -->
             </tr>
             <tr>
               <th>姓名</th>
-              <td>{{ order.user.name }}</td>
+              <!-- <td>{{ order.user.name }}</td> -->
             </tr>
             <tr>
               <th>收件人電話</th>
-              <td>{{ order.user.tel }}</td>
+              <!-- <td>{{ order.user.tel }}</td> -->
             </tr>
             <tr>
               <th>收件人地址</th>
-              <td>{{ order.user.address }}</td>
+              <!-- <td>{{ order.user.address }}</td> -->
             </tr>
             <tr>
               <th>付款狀態</th>
               <td>
-                <span v-if="!order.is_paid">尚未付款</span>
-                <span v-else class="text-success">付款完成</span>
+                <!-- <span v-if="!order.is_paid">尚未付款</span> -->
+                <!-- <span v-else class="text-success">付款完成</span> -->
               </td>
             </tr>
           </tbody>
         </table>
-        <div class="text-right" v-if="order.is_paid === false">
+        <!-- <div class="text-right" v-if="order.is_paid === false">
           <button class="btn btn-danger">確認付款去</button>
-        </div>
+        </div> -->
       </form>
     </div>
   </div>
@@ -200,6 +200,17 @@ export default {
       cart:{},
       status:{
         loadingItem: '',
+      },
+      form:{
+        data:{
+          user: {
+          name: "",
+          email: "",
+          tel: "",
+          address: ""
+        },
+        message: ""
+        }
       },
       isLoading:false,
       coupon_code:"",
